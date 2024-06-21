044c4e2d7ae0c80906d1979381a92bcebce40785
Cameron Harrison <54902084+cambo602@users.noreply.github.com>
2024-06-21 14:12:27 -0400

    Check sum stuff in rec, very cool ! 100% works no need to debug this!!!

--- packet_revciver.py
+++ packet_revciver.py
@@ -6,4 +6,38 @@ serverSocket.listen(5)
 
 while True:
   (clientSocket, address) = serverSocket.accept()
-  print(clientSocket.recv(1024))
+  data = clientSocket.recv(1024)
+
+  if data != '':
+    header = data[:41]
+    message = data[40:]
+
+    # split header into 4 byte hex values
+    hex_header = [header[i:i+4] for i in range(0, len(header), 4)]
+    check_sum = hex_header[5]
+    hex_header.remove(5)
+
+    # add hex values together excluding the 6th one(thats the check sum)
+    sum = 0
+    for hex in hex_header:
+      sum += int(hex, 16)
+    
+    hex_sum = hex(sum)
+
+    #add overflow to the sum
+    if len(hex_sum) > 4:
+      sum = int(hex_sum[1:], 16) + int(hex_sum[:1], 16)
+      hex_sum = hex(sum)
+    
+    # get the ones complement of the hex_sum
+    ones_complement = hex(0xFFFF - int(hex_sum, 16))
+
+    # check if the check sum is correct
+    if ones_complement == check_sum:
+      print(message)
+    else:
+      print('Check sum failed')
+
+    
+
+  
